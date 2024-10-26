585f904efb0fe2dee49a6b2fb6b3ed3f2947e7e7
Shorebirds <info@drinkshorebirds.com>
2024-10-26 16:49:42 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
-// Add your API key from Coinbase Commerce
-export const COINBASE_COMMERCE_API_KEY =
+export const COINBASE_COMMERCE_API_KEY = "38d41058-2113-4986-a616-696cbd745774"
   process.env.NEXT_PUBLIC_COINBASE_COMMERCE_API_KEY || '';
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : 'https://onchain-commerce-template.vercel.app';
-// Add your API KEY from the Coinbase Developer Portal
-export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
+export const NEXT_PUBLIC_CDP_API_KEY = "qAVfQloW6mgPMuA2AIfiwNyrlUr8imY9" process.env.NEXT_PUBLIC_CDP_API_KEY;
